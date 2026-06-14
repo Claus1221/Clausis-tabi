f72bcafe3eefb8fcb96c92fbdf072aa6fbac8148
Claus1221 <clausalbers@gmail.com>
2026-06-14 10:54:04 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TabiApp from './TabiApp.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TabiApp />
+  </React.StrictMode>
+)
